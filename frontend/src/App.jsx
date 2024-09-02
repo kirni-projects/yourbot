befbf6af710466d1bd7bd3c6d616021a2b77d71b
Kiran <kiranchauhan162k@gmail.com>
2024-09-02 18:23:02 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Register />} />
+                <Route path="*" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/script-check" element={<ScriptCheck />} /> {/* Adjust path as needed */}
             </Routes>
